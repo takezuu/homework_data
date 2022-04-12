2e4f4f9351f5e83c26570cdfac6cbeb16403cc98
takezuu <petrov-nikita_07@mail.ru>
2022-04-12 13:29:22 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ JSON_HEADER = ('name', 'gender', 'address', 'age', 'books')
 all_books = []
 all_users = []
 
-#Создаю из csv файла список со всеми книгами
+# Создаю из csv файла список со всеми книгами
 with open('books.csv', newline='') as file:
     csv_file = csv.DictReader(file)
     for row in csv_file:
@@ -14,7 +14,7 @@ with open('books.csv', newline='') as file:
         csv_dict = (dict(zip(HEADER, new_row)))
         all_books.append(csv_dict)
 
-#Из json файла создаю список со всеми пользователями
+# Из json файла создаю список со всеми пользователями
 with open('users.json', 'r') as file:
     json_file = json.load(file)
     for row in json_file:
@@ -22,7 +22,7 @@ with open('users.json', 'r') as file:
         new_json_dict = (dict(zip(JSON_HEADER, new_json_row)))
         all_users.append(new_json_dict)
 
-#Раздача книг пользователям
+# Раздача книг пользователям
 number_of_users = len(all_users)
 user_count = 0
 for book in all_books:
@@ -31,17 +31,6 @@ for book in all_books:
     if user_count >= number_of_users:
         user_count = 0
 
-
-#Запись в финальный файл пользователей с розданными книгами
+# Запись в финальный файл пользователей с розданными книгами
 with open('result.json', 'w') as final_file:
     json.dump(all_users, final_file, indent=4)
-
-
-
-
-
-
-
-
-
-
